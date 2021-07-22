d9b052096ffa470f6017c870f21775188451a5ce
Dagmar87 <jdfssobrinho@hotmail.com>
2021-07-22 10:38:33 -0300

    Adicionando validação de campos do formulário

--- index.php
+++ index.php
@@ -1,43 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
-$categorias[] = 'idoso';
+<head>
+    <meta charset="utf-8">
+    <title>Formulário de inscrição</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width-device-width, initial-scale-1">
+</head>
 
-//print_r($categorias);
+<body>
 
-$nome = 'Dagmar';
-$idade = 26;
+<p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
-//var_dump($nome);
-//var_dump($idade);
+<form action="script.php" method="post">
+    <p>Seu nome: <input type="text" name="nome" /></p>
+    <p>Sua idade: <input type="text" name="idade" /></p>
+    <p><input type="submit" value="Enviar os dados do competidor" /></p>
+</form>
 
-if($idade >= 6 && $idade <= 12)
-{
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'infantil'){
-            echo "O nadador ".$nome, " compete na categoria " .$categorias[$i]; 
-        }            
-    } 
-}
-else if($idade >= 13 && $idade <= 18)
-{
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'adolescente'){
-            echo "O nadador ".$nome, " compete na categoria " .$categorias[$i]; 
-        }            
-    }
-}
-else
-{
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'adulto'){
-            echo "O nadador ".$nome, " compete na categoria ".$categorias[$i]; 
-        }            
-    }
-}
+</body>
+
+</html>
